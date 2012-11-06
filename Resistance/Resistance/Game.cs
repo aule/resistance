8abfe4d372d4662ae45a09d195bcc02a93610c34
Joseph Taylor <joe.aule@gmail.com>
2012-11-06 14:45:44 +0000

    Implementing Select Spies
    Also refactored tests, added common factory methods to produce games in
    different states. Events which have been uncommented in IGame have been
    added as well. All tests currently pass.
    Tests still to be added to informing players of their role, although
    this will require changes to IPlayer.

--- Resistance/Resistance/Game.cs
+++ Resistance/Resistance/Game.cs
@@ -9,28 +9,30 @@ namespace Resistance
     public class Game : IGame
     {
 
-        public GameState State { get; private set; }
+        public event EventHandler RolesAssigned;
+        public event EventHandler LeaderChanged;
+        public event EventHandler OperativesChosen;
+        public event EventHandler MissionStarting;
+        public event EventHandler MissionCompleted;
+        public event EventHandler GameOver;
 
+        public GameState State { get; private set; }
         public List<IPlayer> Players { get; private set; }
         public List<IMission> Missions { get; private set; }
         public int PlayerCount { get; private set; }
 
         private IPlayer _leader;
-        public event EventHandler LeaderChanged;
         public IPlayer Leader { get { return _leader; } private set { _leader = value; if (LeaderChanged != null) LeaderChanged(this, EventArgs.Empty); } }
+
+        private List<IPlayer> _spies; 
 
         public Game(IEnumerable<IPlayer> players, IEnumerable<IMission> missions )
         {
-            Players = players.ToList();
-            Missions = missions.ToList();
+            Players = (players ?? Enumerable.Empty<IPlayer>()).ToList();
+            Missions = (missions ?? Enumerable.Empty<IMission>()).ToList();
             PlayerCount = Players.Count();
 
-            if(PlayerCount<5 || PlayerCount>10)
-            {
-                throw new ArgumentOutOfRangeException("players", PlayerCount,
-                                                      "Player count must be between 5 and 10.");
-            }
-            State = GameState.SelectingLeader;
+            State = GameState.NotReady;
         }
 
         private bool PlayerIsInGame(IPlayer player)
@@ -38,14 +40,35 @@ namespace Resistance
             return Players.Contains(player);
         }
 
-        public bool SelectLeader(IPlayer player)
+        public bool SelectLeader(IPlayer leader)
         {
             if (State != GameState.SelectingLeader) return false;
-            if (!PlayerIsInGame(player)) return false;
+            if (!PlayerIsInGame(leader)) return false;
 
             State = GameState.SelectingMissionOperatives;
-            Leader = player;
+            Leader = leader;
 
+            return true;
+        }
+
+        private void AnnounceRoles()
+        {
+            
+        }
+
+        public bool SelectSpies(IEnumerable<IPlayer> spies)
+        {
+            if (State != GameState.NotReady) return false;
+            _spies = (spies ?? Enumerable.Empty<IPlayer>()).ToList();
+            if(_spies.All(PlayerIsInGame))
+            {
+                AnnounceRoles();
+                State = GameState.SelectingLeader;
+            } else
+            {
+                _spies = null;
+                return false;
+            }
             return true;
         }
     }
--- Resistance/ResistanceTests/GameTests.cs
+++ Resistance/ResistanceTests/GameTests.cs
@@ -14,6 +14,19 @@ namespace ResistanceTests
     [TestClass]
     public class GameTests
     {
+        private Game PrepareFreshGame(IEnumerable<IPlayer> players, IEnumerable<IMission> missions)
+        {
+            return new Game(players, missions);
+        }
+
+        private Game PrepareGameInSelectLeaderState(IEnumerable<IPlayer> players, IEnumerable<IMission> missions, IEnumerable<IPlayer> spies = null )
+        {
+            Game game = new Game(players, missions);
+            game.SelectSpies(spies);
+            Assert.AreEqual(GameState.SelectingLeader,game.State);
+            return game;
+        }
+
         private IPlayer GenenerateMoqPlayer()
         {
             return new Mock<IPlayer>().Object;
@@ -27,71 +40,110 @@ namespace ResistanceTests
         [TestMethod]
         public void NewGame_TenPlayers_PlayerCountSetToTen()
         {
-            Game game = new Game(GenenerateMoqPlayers(10),null);
-            Assert.AreEqual(10, game.PlayerCount, "Player count should be set at game start.");
+            Game game = PrepareFreshGame(GenenerateMoqPlayers(10), null);
+            Assert.AreEqual(10, game.PlayerCount);
         }
 
         [TestMethod]
-        public void NewGame_ValidPlayerCount_GameStateIsSelectingLeader()
+        public void NewGame_GameStateIsNotReady()
         {
-            Game game = new Game(GenenerateMoqPlayers(5), null);
-            Assert.AreEqual(GameState.SelectingLeader, game.State, "Initial game state is 'selecting leader'.");
+            Game game = PrepareFreshGame(null, null);
+            Assert.AreEqual(GameState.NotReady, game.State);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void NewGame_FourPlayers_ThrowsArgumentOutOfRangeException()
-        {
-            Game game = new Game(GenenerateMoqPlayers(4), null);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void NewGame_ElevenPlayers_ThrowsArgumentOutOfRangeException()
-        {
-            Game game = new Game(GenenerateMoqPlayers(11), null);
-        }
-
-        [TestMethod]
-        public void SelectLeader_StateIsSelectingLeader_ReturnsTrue()
+        public void SelectSpies_ListContainsOnlyNonPlayers_ReturnsFalse()
         {
             List<IPlayer> players = GenenerateMoqPlayers(6);
-            Game game = new Game(players);
-            Assert.AreEqual(GameState.SelectingLeader, game.State);
-            Assert.IsTrue(game.SelectLeader(players[5]), null);
+            Game game = PrepareFreshGame(players, null);
+            Assert.IsFalse(game.SelectSpies(Enumerable.Range(1, 3).Select(n => GenenerateMoqPlayer())));
         }
 
         [TestMethod]
-        public void SelectLeader_StateIsSelectingLeader_StateChangesToSelectingMissionOperatives()
+        public void SelectSpies_ListContainsSomeNonPlayers_ReturnsFalse()
+        {
+            List<IPlayer> players = GenenerateMoqPlayers(6);
+            IEnumerable<IPlayer> spies = players.Take(2).Concat(Enumerable.Range(1, 2).Select(n => GenenerateMoqPlayer()));
+            Game game = PrepareFreshGame(players, null);
+            Assert.IsFalse(game.SelectSpies(spies));
+        }
+
+        [TestMethod]
+        public void SelectSpies_InvalidSpies_StateRemainsNotReady()
+        {
+            List<IPlayer> players = GenenerateMoqPlayers(6);
+            Game game = PrepareFreshGame(players, null);
+            game.SelectSpies(Enumerable.Range(1, 3).Select(n => GenenerateMoqPlayer()));
+            Assert.AreEqual(GameState.NotReady,game.State);
+        }
+
+        [TestMethod]
+        public void SelectSpies_ValidSpies_ReturnsTrue()
+        {
+            List<IPlayer> players = GenenerateMoqPlayers(8);
+            IEnumerable<IPlayer> spies = players.Skip(2).Take(3);
+            Game game = PrepareFreshGame(players, null);
+            Assert.IsTrue(game.SelectSpies(spies));
+        }
+
+        [TestMethod]
+        public void SelectSpies_ValidSpies_StateChangesToSelectingLeader()
+        {
+            List<IPlayer> players = GenenerateMoqPlayers(8);
+            IEnumerable<IPlayer> spies = players.Skip(4).Take(2);
+            Game game = PrepareFreshGame(players, null);
+            game.SelectSpies(spies);
+            Assert.AreEqual(GameState.SelectingLeader, game.State);
+        }
+
+        [TestMethod]
+        public void SelectSpies_StateIsntNotReady_ReturnsFalse()
+        {
+            Game game = PrepareGameInSelectLeaderState(null, null);
+            Assert.AreNotEqual(GameState.NotReady, game.State);
+            Assert.IsFalse(game.SelectSpies(null));
+        }
+
+        [TestMethod]
+        public void SelectLeader_ValidLeader_StateChangesToSelectingMissionOperatives()
         {
             List<IPlayer> players = GenenerateMoqPlayers(7);
-            Game game = new Game(players, null);
+            Game game = PrepareGameInSelectLeaderState(players, null);
             game.SelectLeader(players[5]);
             Assert.AreEqual(GameState.SelectingMissionOperatives, game.State);
+        }
+
+        [TestMethod]
+        public void SelectLeader_ValidLeader_ReturnsTrue()
+        {
+            List<IPlayer> players = GenenerateMoqPlayers(6);
+            Game game = PrepareGameInSelectLeaderState(players, null);
+            Assert.AreEqual(GameState.SelectingLeader, game.State);
+            Assert.IsTrue(game.SelectLeader(players[5]), null);
         }
 
         [TestMethod]
         public void SelectLeader_StateIsNotSelectingLeader_ReturnsFalse()
         {
             List<IPlayer> players = GenenerateMoqPlayers(6);
-            Game game = new Game(players, null);
+            Game game = PrepareGameInSelectLeaderState(players, null);
             game.SelectLeader(players[5]);
             Assert.AreNotEqual(GameState.SelectingLeader,game.State);
             Assert.IsFalse(game.SelectLeader(players[5]));
         }
 
         [TestMethod]
-        public void SelectLeader_PlayerIdNotInGame_ReturnsFalse()
+        public void SelectLeader_PlayerNotInGame_ReturnsFalse()
         {
-            Game game = new Game(GenenerateMoqPlayers(9), null);
+            Game game = PrepareGameInSelectLeaderState(GenenerateMoqPlayers(9), null);
             Assert.AreEqual(GameState.SelectingLeader, game.State);
             Assert.IsFalse(game.SelectLeader(GenenerateMoqPlayer()));
         }
 
         [TestMethod]
-        public void SelectLeader_PlayerIdNotInGame_StateIsUnchanged()
+        public void SelectLeader_PlayerNotInGame_StateIsUnchanged()
         {
-            Game game = new Game(GenenerateMoqPlayers(8), null);
+            Game game = PrepareGameInSelectLeaderState(GenenerateMoqPlayers(8), null);
             Assert.AreEqual(GameState.SelectingLeader, game.State);
             Assert.IsFalse(game.SelectLeader(GenenerateMoqPlayer()));
             Assert.AreEqual(GameState.SelectingLeader, game.State);
@@ -102,7 +154,7 @@ namespace ResistanceTests
         {
             List<IPlayer> players = GenenerateMoqPlayers(7);
             IPlayer expectedLeader = players[3];
-            Game game = new Game(players, null);
+            Game game = PrepareGameInSelectLeaderState(players, null);
             game.SelectLeader(expectedLeader);
             Assert.AreEqual(expectedLeader, game.Leader);
         }
@@ -111,7 +163,7 @@ namespace ResistanceTests
         public void SelectLeader_ValidLeader_LeaderChangedEventFired()
         {
             List<IPlayer> players = GenenerateMoqPlayers(10);
-            Game game = new Game(players, null);
+            Game game = PrepareGameInSelectLeaderState(players, null);
             int eventCalled = 0;
             game.LeaderChanged += (o, i) => { eventCalled++; };
             game.SelectLeader(players[7]);
@@ -122,7 +174,7 @@ namespace ResistanceTests
         public void SelectLeader_PlayerIdNotInGame_LeaderChangedEventNotFired()
         {
             List<IPlayer> players = GenenerateMoqPlayers(6);
-            Game game = new Game(players, null);
+            Game game = PrepareGameInSelectLeaderState(players, null);
             int eventCalled = 0;
             game.LeaderChanged += (o, i) => { eventCalled++; };
             game.SelectLeader(GenenerateMoqPlayer());
