9f534b54f8bc3e05b93953f27e9a4b954d4b2519
Joseph Taylor <joe.aule@gmail.com>
2012-11-13 15:41:15 +0000

    Implemented Vote class and related tests
    Created a simple implementation of IVote which returns true for a simple
    majority and false for an even split or less.

---
+++ Resistance/Resistance/Vote.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resistance
+{
+    public class Vote : IVote
+    {
+        private TaskCompletionSource<bool> _voteCompletionSource;
+        private int _votesRemaining;
+        private int _yesVotes;
+        private int _noVotes;
+
+        public Task<bool> CallVote(IEnumerable<IVoter> voters)
+        {
+            List<IVoter> votersList = (voters ?? Enumerable.Empty<IVoter>()).ToList();
+            _votesRemaining = votersList.Count;
+            _yesVotes = _noVotes = 0;
+            _voteCompletionSource = new TaskCompletionSource<bool>();
+            foreach (IVoter voter in votersList)
+            {
+                voter.RequestVote().ContinueWith(CastVote);
+            }
+            return _voteCompletionSource.Task;
+        }
+
+        private void CastVote( Task<bool> vote )
+        {
+            if (vote.Result)
+            {
+                Interlocked.Increment(ref _yesVotes);
+            } else
+            {
+                Interlocked.Increment(ref _noVotes);
+            }
+            if (Interlocked.Decrement(ref _votesRemaining) == 0)
+            {
+                CountVote();
+            }
+        } 
+
+        private void CountVote()
+        {
+            bool result = _yesVotes > _noVotes;
+            _voteCompletionSource.TrySetResult(result);
+        }
+    }
+}
---
+++ Resistance/ResistanceTests/VoteTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Resistance;
+
+namespace ResistanceTests
+{
+    /// <summary>
+    /// Tests for the voting system (implmenting IVote) used to decide on which operatives are sent on missions
+    /// </summary>
+    [TestClass]
+    public class VoteTests
+    {
+        private Vote PrepareFreshVote()
+        {
+            return new Vote();
+        }
+
+        private List<Mock<IVoter>> GenenerateVoterMocks(int voterCount)
+        {
+            return Enumerable.Range(1, voterCount).Select(n => new Mock<IVoter>()).ToList();
+        }
+
+        private List<TaskCompletionSource<bool>> SetupRequestVoteMethods(IEnumerable<Mock<IVoter>> mockVoters)
+        {
+            return mockVoters.Select(mock =>
+                                  {
+                                      TaskCompletionSource<bool> votingCompletionSource = new TaskCompletionSource<bool>();
+                                      mock.Setup(voter => voter.RequestVote()).Returns(votingCompletionSource.Task);
+                                      return votingCompletionSource;
+                                  }).ToList();
+        }
+        
+        [TestMethod]
+        public void CallVote_FreshVote_ReturnsTaskWaitingForActivation()
+        {
+            Vote vote = PrepareFreshVote();
+            Task<bool> task = vote.CallVote(null);
+            Assert.AreEqual(TaskStatus.WaitingForActivation, task.Status);
+        }
+
+        [TestMethod]
+        public void CallVote_WithVoters_RequestVoteCalledOnAllVoters()
+        {
+            Vote vote = PrepareFreshVote();
+            List<Mock<IVoter>> voterMocks = GenenerateVoterMocks(6);
+            IEnumerable<IVoter> voters = voterMocks.Select(mock => mock.Object);
+            List<TaskCompletionSource<bool>> votingCompletionSources = SetupRequestVoteMethods(voterMocks);
+            vote.CallVote(voters);
+            voterMocks.ForEach(voterMock => voterMock.Verify(voter => voter.RequestVote(), Times.Once()));
+        }
+
+        [TestMethod]
+        public void CallVote_AllVotersReturnTrue_VoteCompletesTrue()
+        {
+            Vote vote = PrepareFreshVote();
+            List<Mock<IVoter>> voterMocks = GenenerateVoterMocks(6);
+            IEnumerable<IVoter> voters = voterMocks.Select(mock => mock.Object);
+            List<TaskCompletionSource<bool>> votingCompletionSources = SetupRequestVoteMethods(voterMocks);
+            Task<bool> votingTask = vote.CallVote(voters);
+            votingCompletionSources.ForEach(votingCompletionSource => votingCompletionSource.SetResult(true));
+            // Allow 100ms for the vote counting code to react to the asyncronous voting, fail if timeout occurs
+            Assert.IsTrue(votingTask.Wait(100));
+            Assert.IsTrue(votingTask.Result);
+        }
+
+        [TestMethod]
+        public void CallVote_AllVotersReturnFalse_VoteCompletesFalse()
+        {
+            Vote vote = PrepareFreshVote();
+            List<Mock<IVoter>> voterMocks = GenenerateVoterMocks(6);
+            IEnumerable<IVoter> voters = voterMocks.Select(mock => mock.Object);
+            List<TaskCompletionSource<bool>> votingCompletionSources = SetupRequestVoteMethods(voterMocks);
+            Task<bool> votingTask = vote.CallVote(voters);
+            votingCompletionSources.ForEach(votingCompletionSource => votingCompletionSource.SetResult(false));
+            // Allow 100ms for the vote counting code to react to the asyncronous voting, fail if timeout occurs
+            Assert.IsTrue(votingTask.Wait(100));
+            Assert.IsFalse(votingTask.Result);
+        }
+
+        [TestMethod]
+        public void CallVote_VoteSplitEvenly_VoteCompletesFalse()
+        {
+            Vote vote = PrepareFreshVote();
+            List<Mock<IVoter>> trueVoterMocks = GenenerateVoterMocks(5);
+            List<Mock<IVoter>> falseVoterMocks = GenenerateVoterMocks(5);
+            IEnumerable<IVoter> voters = trueVoterMocks.Concat(falseVoterMocks).Select(mock => mock.Object);
+            List<TaskCompletionSource<bool>> trueVotingCompletionSources = SetupRequestVoteMethods(trueVoterMocks);
+            List<TaskCompletionSource<bool>> falseVotingCompletionSources = SetupRequestVoteMethods(falseVoterMocks);
+            Task<bool> votingTask = vote.CallVote(voters);
+            trueVotingCompletionSources.ForEach(votingCompletionSource => votingCompletionSource.SetResult(true));
+            falseVotingCompletionSources.ForEach(votingCompletionSource => votingCompletionSource.SetResult(false));
+            // Allow 100ms for the vote counting code to react to the asyncronous voting, fail if timeout occurs
+            Assert.IsTrue(votingTask.Wait(100));
+            Assert.IsFalse(votingTask.Result);
+        }
+
+        [TestMethod]
+        public void CallVote_MoreTrueVotesThanFalseVotes_VoteCompletesTrue()
+        {
+            Vote vote = PrepareFreshVote();
+            List<Mock<IVoter>> trueVoterMocks = GenenerateVoterMocks(6);
+            List<Mock<IVoter>> falseVoterMocks = GenenerateVoterMocks(5);
+            IEnumerable<IVoter> voters = trueVoterMocks.Concat(falseVoterMocks).Select(mock => mock.Object);
+            List<TaskCompletionSource<bool>> trueVotingCompletionSources = SetupRequestVoteMethods(trueVoterMocks);
+            List<TaskCompletionSource<bool>> falseVotingCompletionSources = SetupRequestVoteMethods(falseVoterMocks);
+            Task<bool> votingTask = vote.CallVote(voters);
+            trueVotingCompletionSources.ForEach(votingCompletionSource => votingCompletionSource.SetResult(true));
+            falseVotingCompletionSources.ForEach(votingCompletionSource => votingCompletionSource.SetResult(false));
+            // Allow 100ms for the vote counting code to react to the asyncronous voting, fail if timeout occurs
+            Assert.IsTrue(votingTask.Wait(100));
+            Assert.IsTrue(votingTask.Result);
+        }
+
+    }
+}
